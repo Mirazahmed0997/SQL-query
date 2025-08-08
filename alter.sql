cd0aa67ab21f25b75ea77796cd8f62ad01038ca7
Miraz Ahmed <ahmedmiraz87@gmail.com>
2025-08-09 03:41:20 +0600

    DDL query

--- alter.sql
+++ alter.sql
@@ -1,24 +1,57 @@
 -- Active: 1754609574546@@127.0.0.1@5432@ph
-SELECT * FROM person1;
 
-ALTER TABLE person1
+
+CREATE TABLE person2(
+
+id serial PRIMARY KEY,
+user_name VARCHAR(20) NOT NULL,
+age INT CHECK (age>=18)
+)
+
+
+SELECT * FROM person2;
+
+ALTER TABLE person2
     ADD COLUMN email VARCHAR(50) DEFAULT 'default@gmail.com' NOT NULL;
 
-    INSERT INTO person1 VALUES(2,'test2',45,'test2@gmail.com');
+    INSERT INTO person2 VALUES(2,'test3',60,'test2@gmail.com');
 
-    ALTER TABLE person1
+    ALTER TABLE person2
     DROP COLUMN email ;
 
 
-    alter Table person1
+    alter Table person2
         RENAME COLUMN age to user_age;
 
-    alter TABLE person1
+    alter TABLE person2
         alter COLUMN user_name  TYPE VARCHAR(80)
 
-    alter Table person1
+    alter Table person2
         alter COLUMN user_age set NOT NULL;   
 
 
-    alter Table person1
+    alter Table person2
         alter COLUMN user_age DROP NOT NULL;    
+
+    ALTER TABLE person2
+        ADD constraint unique_person2_user_name UNIQUE(user_name);
+
+
+    ALTER TABLE person2
+        ADD constraint pk_person2_user_name PRIMARY KEY(user_name);
+
+
+    ALTER TABLE person2
+        DROP constraint unique_person2_user_name
+
+    DROP TABLE person1;
+
+
+    TRUNCATE TABLE person2;
+
+
+    
+
+    
+
+    
