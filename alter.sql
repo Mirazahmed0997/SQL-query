8809369496eab414b46c1fd4c5f675b3cb4e7410
Miraz Ahmed <ahmedmiraz87@gmail.com>
2025-08-08 06:10:56 +0600

    add constraint

--- alter.sql
+++ alter.sql
@@ -2,6 +2,23 @@
 SELECT * FROM person1;
 
 ALTER TABLE person1
-    ADD COLUMN email VARCHAR(50) NOT NULL;
+    ADD COLUMN email VARCHAR(50) DEFAULT 'default@gmail.com' NOT NULL;
 
-    INSERT INTO person1 VALUES(1,'test',45,'test@gmail.com');
+    INSERT INTO person1 VALUES(2,'test2',45,'test2@gmail.com');
+
+    ALTER TABLE person1
+    DROP COLUMN email ;
+
+
+    alter Table person1
+        RENAME COLUMN age to user_age;
+
+    alter TABLE person1
+        alter COLUMN user_name  TYPE VARCHAR(80)
+
+    alter Table person1
+        alter COLUMN user_age set NOT NULL;   
+
+
+    alter Table person1
+        alter COLUMN user_age DROP NOT NULL;    
