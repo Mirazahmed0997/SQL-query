0a493b2f04b153be3039ca255123b11ab0e17538
Miraz Ahmed <ahmedmiraz87@gmail.com>
2025-08-12 03:32:42 +0600

    groupClawsSQL

---
+++ groupBy.sql
@@ -0,0 +1,22 @@
+
+
+SELECT *FROM student;
+
+
+SELECT country,count(*),avg(age) FROM student
+    GROUP BY country
+
+
+SELECT country,avg(age) FROM student
+    GROUP BY country
+        HAVING avg(age)<30
+
+SELECT EXTRACT(YEAR FROM dob),COUNT(*) FROM student
+    GROUP BY dob
+      
+
+
+
+
+
+ 
